0ff1b589ab4dded22fe36fe24fe3d2fff920d6d0
mcdivine2 <markdave.canoy@sccpag.edu.ph>
2024-10-09 20:20:06 +0800

    Update class_model.php
    asdf

--- init/model/class_model.php
+++ init/model/class_model.php
@@ -419,7 +419,7 @@
 			// Ensure the connection is active
 			if ($this->conn->ping()) {
 				// Prepare the SQL statement
-				$stmt = $this->conn->prepare("INSERT INTO tbl_documentrequest (first_name, last_name, complete_address, birthdate, course, email_address, control_no, document_name, no_ofcopies, date_request, registrar_status, purpose, mode_request, student_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				$stmt = $this->conn->prepare("INSERT INTO tbl_documentrequest (first_name, middle_name, last_name, complete_address, birthdate, course, email_address, control_no, document_name, no_ofcopies, date_request, status, purpose, mode_request, student_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 				
 				if ($stmt === false) {
 					die('Prepare failed: (' . $this->conn->errno . ') ' . $this->conn->error);
