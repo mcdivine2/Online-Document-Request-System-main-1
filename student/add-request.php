150f682a4679d54b8e6a57af0cef4dfda40124f4
mcdivine2 <146824292+mcdivine2@users.noreply.github.com>
2024-10-14 18:57:42 +0800

    Reapply "Merge pull request #4 from mcdivine2/macaya-works-sub"
    This reverts commit 1b41b0233f808c4f642b8b508fa35dbe6a21ba9d.

--- student/add-request.php
+++ student/add-request.php
@@ -136,7 +136,7 @@
                             </div>
                             <div class="col-md-6">
                                 <label>Email Address</label>
-                                <input type="email" name="email_address" class="form-control" placeholder="Enter Email">
+                                <input type="email" name="email_address" value="<?= $getstudno['email_address']; ?>" class="form-control" placeholder="">
                             </div>
                         </div>
 
